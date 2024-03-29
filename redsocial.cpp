9fd268da6c6c90d56030f47b07520086e0f729cf
dani24gh <19047@portalcedi.edu.mx>
2024-03-29 15:43:30 -0600

    a

--- redsocial.cpp
+++ redsocial.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "redsocial.h"
 
 using namespace std;
 
 void RedSocial::agregarUsuario(){
+ string nombreUsuario;
+    cout << "Ingresa el nombre del nuevo usuario: ";
+    cin >> nombreUsuario;
 
+    Usuario usuario(nombreUsuario);
+    usuarios.push_back(usuario);
+    numeroDeUsuarios++;
+    cout << "Usuario " << nombreUsuario << " agregado correctamente." << endl;
 }
 
 void RedSocial::mostrarUsuarios(){
+        cout << "Usuarios en Nonsense" << ":" << endl;
+    for (Usuario& usuario : usuarios) {
+        cout << "- " << usuario.nombre << endl;
+    }
 
 }
-
 
 void RedSocial::mostrarPublicaciones(){
     for(int i=0; i<this->publicaciones.size(); i++){
@@ -21,17 +32,33 @@ void RedSocial::mostrarPublicaciones(){
 }
 
 Usuario* RedSocial::getUsuario(int id){
-
+for (Usuario& usuario : usuarios) {
+        if (usuario.id == id) {
+            return &usuario;
+        }
+}
+   cout << "No se encontró un usuario con el ID " << id << endl;
+    return 0;
 }
 
 RedSocial::RedSocial(string nombre){
-    
+    this->nombre = nombre;
+    this->numeroDeUsuarios = 0;
+    this->numeroDePublicaciones = 0;
+
 }
 
 RedSocial::RedSocial(string nombre, vector<Usuario*> usuarios){
-
+ this->nombre = nombre;
+    this->usuarios = usuarios;
+    this->numeroDeUsuarios = usuarios.size();
+    this->numeroDePublicaciones = 0;
 }
 
 RedSocial::RedSocial(string nombre, vector<Usuario*> usuarios, vector<Publicacion*> publicaciones){
-
+this->nombre = nombre;
+    this->usuarios = usuarios;
+    this->publicaciones = publicaciones;
+    this->numeroDeUsuarios = usuarios.size();
+    this->numeroDePublicaciones = publicaciones.size();
 }
